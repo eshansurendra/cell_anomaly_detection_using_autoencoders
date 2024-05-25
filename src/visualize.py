afc2b9492d3f13e6868d0f09931694b16dc4fc4b
Eshan Surendra <107266456+eshansurendra@users.noreply.github.com>
2024-05-25 22:54:32 +0530

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -1,26 +1,13 @@
-# visualize.py 
-
 import matplotlib.pyplot as plt
-import random 
-import glob
-from main import preprocess_image, load_model 
 
-def visualize_reconstructions(autoencoder_model, image_paths, num_images=5):
-    """Visualizes original and reconstructed images from a directory."""
-    fig, axs = plt.subplots(2, num_images, figsize=(15, 6))
-    random_indices = random.sample(range(len(image_paths)), num_images)
-    for i, idx in enumerate(random_indices):
-        img_path = image_paths[idx]
-        original_img = preprocess_image(img_path)
-        reconstructed_img = autoencoder_model.predict(original_img)
-        axs[0, i].imshow(original_img[0])
-        axs[0, i].axis('off')
-        axs[1, i].imshow(reconstructed_img[0])
-        axs[1, i].axis('off')
+def plot_loss(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'y', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
     plt.show()
-
-if __name__ == "__main__":
-    model = load_model() 
-    image_dir = 'cell_images2/uninfected_train/images/' # Example directory 
-    image_paths = glob.glob(image_dir + '*') 
-    visualize_reconstructions(model, image_paths) 
